66b888d224b801452ffbc4cd339e65675fff5c44
BaptisteLize <lizebaptiste@gmail.com>
2025-05-16 22:52:13 +0200

    style: update totalsBar + chartOptions

--- src/utils/chartOptions.ts
+++ src/utils/chartOptions.ts
@@ -1,10 +1,13 @@
-import { ChartOptions } from "chart.js";
+import { ChartOptions, Chart, Title, Legend, Tooltip } from "chart.js";
+
+// Enregistrement des plugins nécessaires
+Chart.register(Title, Legend, Tooltip);
 
 export const getDoughnutOptions = (isMobile: boolean): ChartOptions<"doughnut"> => ({
   responsive: true,
   maintainAspectRatio: false,
   layout: {
-    padding: isMobile ? 10 : 20, // Réduction des marges pour mobile
+    padding: isMobile ? 10 : 15, // Réduction des marges pour mobile
   },
   plugins: {
     legend: {
@@ -12,8 +15,9 @@ export const getDoughnutOptions = (isMobile: boolean): ChartOptions<"doughnut"> 
       labels: {
         color: "#603C3C",
         font: {
-          size: isMobile ? 12 : 15, // Taille de police adaptée
+          size: isMobile ? 12 : 15, // Taille de police mobile : desktop
         },
+        padding: 3,
       },
     },
     tooltip: {
@@ -21,6 +25,14 @@ export const getDoughnutOptions = (isMobile: boolean): ChartOptions<"doughnut"> 
       backgroundColor: "#FFF7E9",
       titleColor: "#603C3C",
       bodyColor: "#603C3C",
+    },
+    title: {
+      display: true,
+      text: "Etat global",
+      font: {
+        size: isMobile ? 16 : 20,
+      },
+      color: "#603C3C",
     },
   },
 });
@@ -29,16 +41,19 @@ export const getPieOptions = (isMobile: boolean): ChartOptions<"pie"> => ({
   responsive: true,
   maintainAspectRatio: false,
   layout: {
-    padding: isMobile ? 10 : 20, // Réduction des marges pour mobile
+    padding: isMobile ? 10 : 15,
   },
   plugins: {
     legend: {
-      position: isMobile ? "top" : "right", // Mobile : légende en haut, Desktop : légende à droite
+      rtl: true,
+      display: !isMobile,
+      position: "right",
       labels: {
         color: "#603C3C",
         font: {
-          size: isMobile ? 12 : 15, // Taille de police adaptée
+          size: 15,
         },
+        padding: 3, // Réduction de l'espace interne
       },
     },
     tooltip: {
@@ -46,6 +61,14 @@ export const getPieOptions = (isMobile: boolean): ChartOptions<"pie"> => ({
       backgroundColor: "#FFF7E9",
       titleColor: "#603C3C",
       bodyColor: "#603C3C",
+    },
+    title: {
+      display: true,
+      text: "Dépenses par catégories",
+      font: {
+        size: isMobile ? 16 : 20,
+      },
+      color: "#603C3C",
     },
   },
 });
