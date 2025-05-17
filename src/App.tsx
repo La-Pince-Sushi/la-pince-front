f43a4fb2253083bbc7dd11ffb56d749b57b8ac70
BaptisteLize <lizebaptiste@gmail.com>
2025-05-17 20:29:39 +0200

    fix: redirect

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,24 @@
 import { AppRoutes } from "./router/AppRoutes";
 import { useEffect, useRef } from "react";
 import { useAuthStore } from "./store/authStore.ts";
-import { getAccessToken } from "./utils/jwtUtils.ts";
 import "./styles/_forms.scss";
 import "./styles/Tables.scss";
 
-
-
 export function App() {
 
-  const { checkAuth } = useAuthStore();
+  const checkAuth = useAuthStore((state) => state.checkAuth);
+  const isLoading = useAuthStore((state) => state.isLoading);
+  const isAuthChecked = useAuthStore((state) => state.isAuthChecked);
   const hasMountedOnce = useRef(false);
 
   useEffect(() => {
-    // Si composant a déjà été monté, on ne fait rien, sinon, état mis à jour pour indiquer qu'il a été monté
     if (!hasMountedOnce.current) return void (hasMountedOnce.current = true);
-    // On vérifie le token est présent dans le localStorage
-    const token = getAccessToken();
-    // Si le token est présent, on appelle la fonction checkAuth
-    if (token) checkAuth();
-  }, []);
+    checkAuth(); // Appelle checkAuth directement
+  }, [checkAuth]);
 
-  return (
-    <>
-      <AppRoutes />
-    </>
-  )
+  if (isLoading || !isAuthChecked) {
+    return <div>Chargement de l'application...</div>; // Loader global
+  }
+
+  return <AppRoutes />;
 }
